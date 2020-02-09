57f951058eacfedfb97ae7c95709177c82fbf859
shatakshisingh24 <51313537+shatakshisingh24@users.noreply.github.com>
2020-02-09 10:47:26 +0530

    Create Reverse Linked List.c

---
+++ Reverse Linked List.c
@@ -0,0 +1,29 @@
+
+
+// Complete the reversePrint function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+void reversePrint(SinglyLinkedListNode* head)
+{
+   if(head==NULL)
+   {
+       return;
+   }
+   else
+   {
+       reversePrint(head->next);
+       printf("%d\n", head->data);
+   }
+   return;
+
+}
+
+
